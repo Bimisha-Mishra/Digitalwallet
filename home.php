596d432a6a1bea2fdd7526d7a03fd6c526fe2494
Rojal Maharjan <rojalmaharjan977@gmail.com>
2022-04-18 23:10:03 +0200

    Home update

--- home.php
+++ home.php
@@ -31,15 +31,16 @@ include("connection.php");
             <div class="icon-container">
                 <i class="fas fa-solid fa-bell"></i>
                 <i class="fa-solid fa-gear"></i>
+                <i class="fa fa-bars"  onClick="showMenu(true)"></i>
             </div>
         </nav>
     </section>
     <!--nav section ends-->
     <!---->
-    <section class="main-container">
-        <div class="wrapper">
-            <aside class="closed" id="sidebar">
-                <ul>
+    <section>
+        <div id="menu__panel">
+            <i id="menu__close" class="menu__close fa fa-angle-left fa-2x" onClick="showMenu(false)"></i>
+            <ul>
                 <li><a href="#"><i class="fa-solid fa-house"></i>Home</a></li>
                 <li><a href="#"><i class="fa-solid fa-coins"></i>Services</a></li>
                 <li><a href="#"><i class="fa-solid fa-calendar-check"></i>Bookings</a></li>
@@ -49,77 +50,10 @@ include("connection.php");
                 <li><a href='#'><i class="fa-solid fa-clock"></i>Transaction History</a></li>
                 <li><a href='#'><i class="fa-solid fa-gift"></i>Coupon</a></li>
                 <li><a href='#'><i class="fa-solid fa-hand-holding-dollar"></i>Loyalty</a></li>
-                </ul>
-            </aside>
-    
-        <main>
-            <div class="menu-icon" id="menu-icon" onclick="toggleNav()">
-            <span></span>
-            </div>
-        </main>
-    </div>
+            </ul>
+        </div>
+        <div class="main-container">
             <!------------------------------->
-            <div class="service-panal">
-                    <div class="services-container">
-                        
-                        <div class="service-box" >
-                            <a class="service-icon" href="LoadFund.html">
-                                
-                                <i class="fa-solid fa-wallet"></i>
-                            </a>
-                            <div class="service-title">Load</div>
-                        </div>
-                        
-                        <div class="service-box">
-                            <div class="service-icon">
-                                <i class="fa-solid fa-money-bill-transfer"></i>
-                            </div>
-                            <div class="service-title">Transfer</div>
-                        </div>
-                        <div class="service-box">
-                            <div class="service-icon">
-                                <i class="fas fa-money-check"></i>
-                            </div>
-                            <div class="service-title">Remittance</div>
-                        </div>
-                        <div class="service-box">
-                            <div class="service-icon">
-                                <i class="fas fa-piggy-bank"></i>
-                            </div>
-                            <div class="service-title">Bank</div>
-                        </div>
-                </div>
-                <!--   ------   -->
-                <ul class="slides">
-                    <input type="radio" name="radio-btn" id="img-1" checked />
-                    <li class="slide-container">
-                        <div class="slide">
-                            <img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
-                        </div>
-                        <div class="nav">
-                            <label for="img-6" class="prev">&#x2039;</label>
-                            <label for="img-2" class="next">&#x203a;</label>
-                        </div>
-                    </li>
-                    
-                    <input type="radio" name="radio-btn" id="img-2" />
-                    <li class="slide-container">
-                        <div class="slide">
-                            <img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
-                        </div>
-                        <div class="nav">
-                            <label for="img-1" class="prev">&#x2039;</label>
-                            <label for="img-3" class="next">&#x203a;</label>
-                        </div>
-                    </li>
-                    
-                    <li class="nav-dots">
-                        <label for="img-1" class="nav-dot" id="img-dot-1"></label>
-                        <label for="img-2" class="nav-dot" id="img-dot-2"></label>
-                    </li>
-                </ul>
-            </div>
-            <!--------------------------->
             <div class="info-panal">
                 <table class="balance"> 
                     <tr>
@@ -221,8 +155,71 @@ include("connection.php");
                     </tr>
                 </table>
             </div>
-        </section>
-        <script src = "script/slidingmenu.js"></script>
-        <script src="https://kit.fontawesome.com/91d850ff13.js" crossorigin="anonymous"></script>
-    </body>
-    </html>
+            
+            <!--------------------------->
+            <div class="service-panal">
+                <div class="services-container">
+                    
+                    <div class="service-box" >
+                        <a class="service-icon" href="LoadFund.html">
+                            
+                            <i class="fa-solid fa-wallet"></i>
+                        </a>
+                        <div class="service-title">Load</div>
+                    </div>
+                    
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fa-solid fa-money-bill-transfer"></i>
+                        </div>
+                        <div class="service-title">Transfer</div>
+                    </div>
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fas fa-money-check"></i>
+                        </div>
+                        <div class="service-title">Remittance</div>
+                    </div>
+                    <div class="service-box">
+                        <div class="service-icon">
+                            <i class="fas fa-piggy-bank"></i>
+                        </div>
+                        <div class="service-title">Bank</div>
+                    </div>
+                </div>
+                <!--   ------   -->
+                <ul class="slides">
+                    <input type="radio" name="radio-btn" id="img-1" checked />
+                    <li class="slide-container">
+                        <div class="slide">
+                            <img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
+                        </div>
+                        <div class="nav">
+                            <label for="img-6" class="prev">&#x2039;</label>
+                            <label for="img-2" class="next">&#x203a;</label>
+                        </div>
+                    </li>
+                    
+                    <input type="radio" name="radio-btn" id="img-2" />
+                    <li class="slide-container">
+                        <div class="slide">
+                            <img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
+                        </div>
+                        <div class="nav">
+                            <label for="img-1" class="prev">&#x2039;</label>
+                            <label for="img-3" class="next">&#x203a;</label>
+                        </div>
+                    </li>
+                    
+                    <li class="nav-dots">
+                        <label for="img-1" class="nav-dot" id="img-dot-1"></label>
+                        <label for="img-2" class="nav-dot" id="img-dot-2"></label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <script src = "script/slidingmenu.js"></script>
+    <script src="https://kit.fontawesome.com/91d850ff13.js" crossorigin="anonymous"></script>
+</body>
+</html>
--- script/slidingmenu.js
+++ script/slidingmenu.js
@@ -1,7 +1,38 @@
-function toggleNav() {
-    var updateElement = document.getElementById("menu-icon");
-    var sidebarElement = document.getElementById("sidebar");
-    //toggle adds a class if it's not there or removes it if it is.
-    updateElement.classList.toggle("open");
-    sidebarElement.classList.toggle("closed");
-}  
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+function showMenu(menu) {
+
+    var angle = '0deg',
+      slide = '300px';
+  
+    if (menu) {
+      angle = '180deg';
+      slide = '0';
+    }
+  
+    // Slide panel
+    $("#menu__panel").css({
+      transform: "translateX(" + slide + ")"
+    });
+  
+    // Rotate icon
+    setTimeout(function() {
+      $("#menu__close").css({
+        transform: "rotate(" + angle + ")"
+      });
+    }, 300);
+  
+    // Animate menu items
+    $(".menu-item").each(function(i) {
+      var row = $(this);
+      setTimeout(function() {
+        menu && row.addClass('fadeInDown');
+        !menu && row.removeClass('fadeInDown');
+      }, 100 * i);
+    });
+  
+  }
